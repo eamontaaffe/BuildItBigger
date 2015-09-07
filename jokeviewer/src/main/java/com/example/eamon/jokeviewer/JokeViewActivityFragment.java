a8f2c27cb53721272179df14874a8879ebace056
eamon <eamontaaffe@gmail.com>
2015-09-07 12:44:48 +1000

    created android library
    still need to add dependencies and utilise the java library from within the android library

---
+++ jokeviewer/src/main/java/com/example/eamon/jokeviewer/JokeViewActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.eamon.jokeviewer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class JokeViewActivityFragment extends Fragment {
+
+    public JokeViewActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_joke_view, container, false);
+    }
+}
