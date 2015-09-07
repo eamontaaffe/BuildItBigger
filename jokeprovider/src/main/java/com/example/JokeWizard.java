785a5c163e563ab33e0060a644b6bc93ee31a244
eamon <eamontaaffe@gmail.com>
2015-09-07 19:11:25 +1000

    deployed to cloud
    finished step 3

--- jokeprovider/src/main/java/com/example/JokeWizard.java
+++ jokeprovider/src/main/java/com/example/JokeWizard.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import java.util.Random;
+
 public class JokeWizard {
     public static String getJoke() {
-        return "Today a man knocked on my door and asked for a small donation towards the local swimming pool. I gave him a glass of water.";
+        return "I never forget A face, but in your case I\'d be glad to make an exception.";
     }
 }
