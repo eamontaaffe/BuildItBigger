3152e125656302f8fcc22b3ec0877366a4941c0e
eamon <eamontaaffe@gmail.com>
2015-09-10 12:13:00 +1000

    improved jokeprovider with a joke api

--- jokeprovider/src/main/java/com/example/JokeWizard.java
+++ jokeprovider/src/main/java/com/example/JokeWizard.java
@@ -1,9 +1,46 @@
 package com.example;
 
-import java.util.Random;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 
 public class JokeWizard {
+    private static String JokeApiUrl = "http://api.icndb.com/jokes/random";
+    private static final String VALUE = "value";
+    private static final String JOKE = "joke";
+
     public static String getJoke() {
-        return "I'm on a whiskey diet. I\'ve lost three days already";
+        String joke = null;
+        try {
+            String jsonString = readUrl(JokeApiUrl);
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONObject valueJson = jsonObject.getJSONObject(VALUE);
+            joke = valueJson.getString(JOKE);
+        } catch (Exception e) {
+
+        } finally {
+            return joke;
+        }
+    }
+
+    private static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
     }
 }
